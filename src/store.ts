e4c8a8bbb863329f145c43ebf79f6257ca50c522
hhimanshu <harit.himanshu@yahoo.in>
2021-10-06 10:38:08 -0700

    set up localstorage middleware

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,19 @@
 import create from "zustand";
+import {persist} from "zustand/middleware";
 
 interface StoreState {
     pagesVisited: number
     incrementFoodPageVisited: () => void
 }
 
-export const useStore = create<StoreState>(set => ({
-    pagesVisited: 0,
-    incrementFoodPageVisited: () => set(state => ({pagesVisited: state.pagesVisited + 1}))
-}))
+export const useStore = create<StoreState>(persist(
+    (set, get) => ({
+        pagesVisited: 0,
+        incrementFoodPageVisited: () => set({ pagesVisited: get().pagesVisited + 1 })
+    }),
+    {
+        name: "page-visits-datastore", // unique name
+        getStorage: () => localStorage
+    }
+))
 
